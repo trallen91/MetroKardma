e5a8beff56d604e88fb05bca70825c725aaf2b4f
trallen91 <travis.william.allen@gmail.com>
2015-10-07 11:15:21 -0400

    complete create_ratings migrtion

--- db/migrate/20151007142822_create_ratings.rb
+++ db/migrate/20151007142822_create_ratings.rb
@@ -1,6 +1,10 @@
 class CreateRatings < ActiveRecord::Migration
   def change
     create_table :ratings do |t|
+      t.references :rater
+      t.references :rated_user
+      t.string :comment, null: false
+      t.integer :score, null: false
 
       t.timestamps null: false
     end
