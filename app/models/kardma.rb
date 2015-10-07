4397d681d6a315ee342ac9639d181b79ef91fdb6
trallen91 <travis.william.allen@gmail.com>
2015-10-07 10:44:29 -0400

    add semi-colons in models to fix syntax

--- app/models/kardma.rb
+++ app/models/kardma.rb
@@ -1,4 +1,4 @@
 class Kardma < ActiveRecord::Base
-  belongs_to :giver, class_name :User
-  belongs_to :receiver, class_name :User
+  belongs_to :giver, class_name: :User
+  belongs_to :receiver, class_name: :User
 end
