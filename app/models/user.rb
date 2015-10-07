f683ce6851a4c778014109e6184ffe3d95f62658
trallen91 <travis.william.allen@gmail.com>
2015-10-07 10:21:12 -0400

    give user secure password and create associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  has_many :given_ratings, class_name :Rating, foreign_key :giver_id
+  has_many :received_ratings, class_name :Rating, foreign_key :receiver_id
+
+  has_many :given_kardmas, class_name :Kardma, foreign_key :giver_id
+  has_many :received_kardmas, class_name :Kardma, foreign_key :receiver_id
 end
