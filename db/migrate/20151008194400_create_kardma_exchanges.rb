4f5cae74578fc085b3b0ea65200fcc5a1d18a43a
trallen91 <travis.william.allen@gmail.com>
2015-10-08 15:56:52 -0400

    complete kardmaexchange migration

--- db/migrate/20151008194400_create_kardma_exchanges.rb
+++ db/migrate/20151008194400_create_kardma_exchanges.rb
@@ -1,6 +1,10 @@
 class CreateKardmaExchanges < ActiveRecord::Migration
   def change
     create_table :kardma_exchanges do |t|
+      t.references :giver
+      t.references :receiver
+      t.references :station
+      t.boolean :complete, default: false
 
       t.timestamps null: false
     end
