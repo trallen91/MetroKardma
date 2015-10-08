0a5d80cef0f31e2466b71ef67d4e16c38126583f
trallen91 <travis.william.allen@gmail.com>
2015-10-08 15:53:43 -0400

    change user models to have many kardma_exchanges

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   has_many :given_ratings, class_name: :Rating, foreign_key: :rater_id
   has_many :received_ratings, class_name: :Rating, foreign_key: :rated_user_id
 
-  has_many :given_kardmas, class_name: :Kardma, foreign_key: :giver_id
-  has_many :received_kardmas, class_name: :Kardma, foreign_key: :receiver_id
+  has_many :given_kardmas, class_name: :KardmaExchange, foreign_key: :giver_id
+  has_many :received_kardmas, class_name: :KardmaExchange, foreign_key: :receiver_id
 
   validates_presence_of :email, :username, :picture
   validates_uniqueness_of :email, :username, :on => :create
