0274b8c8fc980d4f689ab925c7bf994bc4be23c2
Travis Allen <travis.william.allen@gmail.com>
2015-10-15 10:56:45 -0400

    update seed file to reflect devise users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,18 @@
-Station.create(address: "3 Wall Street")
-Station.create(address: "5 Bedford Street")
+wall = Station.create(address: "3 Wall Street")
+bedford = Station.create(address: "5 Bedford Street")
+spring = Station.create(address: "2 Spring Street")
 
-todd = User.create({email: "todd@gmail.com", username: "todd", picture: "me.jpg", password: "password"})
 
-bill = User.create({email: "bill@gmail.com", username: "bill", picture: "bill.jpg", password: "password"})
+todd = User.create({email: "todd@gmail.com", password: "password"})
 
-henry = User.create(email: "henry@gmail.com", username: "henry", picture: "henry.jpg", password: "password")
+bill = User.create({email: "bill@gmail.com", password: "password"})
+
+henry = User.create(email: "henry@gmail.com", password: "password")
+
+jabroni = User.create(email: "henry@gmail.com", password: "password")
 
 KardmaExchange.create(giver: todd, receiver: bill, station: wall, complete: false)
+
+KardmaExchange.create(receiver: henry, station: bedford, complete: false)
+
+KardmaExchange.create(giver: jabroni, station: spring, complete: false)
