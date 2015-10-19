dcb07cf3d4a26a18f71dfa60d307133d23b69d17
Travis Allen <travis.william.allen@gmail.com>
2015-10-19 11:22:30 -0400

    add new create stations migration

---
+++ db/migrate/20151019151907_create_stations.rb
@@ -0,0 +1,10 @@
+class CreateStations < ActiveRecord::Migration
+  def change
+    create_table :stations do |t|
+      t.string :name
+      t.decimal :latitude, {:precision => 10, :scale => 6}
+      t.decimal :longitude, {:precision => 10, :scale => 6}
+      t.timestamps null:false
+    end
+  end
+end
