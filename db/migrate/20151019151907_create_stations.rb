c4dbcaf0df3c846ea6debe4231174eafa432435d
Travis Allen <travis.william.allen@gmail.com>
2015-10-19 15:11:18 -0400

    add lines as attribute for a station

--- db/migrate/20151019151907_create_stations.rb
+++ db/migrate/20151019151907_create_stations.rb
@@ -2,6 +2,7 @@ class CreateStations < ActiveRecord::Migration
   def change
     create_table :stations do |t|
       t.string :name
+      t.string :lines
       t.decimal :latitude, {:precision => 10, :scale => 6}
       t.decimal :longitude, {:precision => 10, :scale => 6}
       t.timestamps null:false
