397c652597d52d5395bdd15d36d704bab57ac08e
Max Schwenk <maschwenk@gmail.com>
2015-10-25 01:10:12 -0400

    Changes for the user authentication needed for Devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,26 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      log_in(@user)
-      redirect_to root_path
-    else
-      #flash errors here
-      redirect_to register_path
-    end
-  end
-
-  def show
-    @user = User.find_by(id: params[:id])
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :username, :password, :picture)
-  end
+  # def new
+  #   @user = User.new
+  # end
+  #
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     log_in(@user)
+  #     redirect_to root_path
+  #   else
+  #     #flash errors here
+  #     redirect_to register_path
+  #   end
+  # end
+  #
+  # def show
+  #   @user = User.find_by(id: params[:id])
+  # end
+  #
+  # private
+  #
+  # def user_params
+  #   params.require(:user).permit(:email, :username, :password, :picture)
+  # end
 end
