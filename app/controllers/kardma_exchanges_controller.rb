81ea8dd48e4a45eaf4932753cf1ae1fa26920133
Travis Allen <travis.william.allen@gmail.com>
2015-10-28 14:08:00 -0400

    add destroy route for kardma_exchanges

--- app/controllers/kardma_exchanges_controller.rb
+++ app/controllers/kardma_exchanges_controller.rb
@@ -4,14 +4,28 @@ class KardmaExchangesController < ApplicationController
     station = Station.find_by(id: params[:station_id])
     #the below line will be simplified once we replace giver and receiver with swiper and swipee in the rails app
     params[:role] == "swiper" ? role = "giver" : role = "receiver"
-    ke = KardmaExchange.new(station:station)
+    @ke = KardmaExchange.new(station:station)
     #the send method allows you to use a variable in place of an attribute name, and the '=' allows you to set that attribute to the value that follows the comma
-    ke.send(role+'=', current_user)
+    @ke.send(role+'=', current_user)
 
-    if ke.save
+    # if current_user.has_pending_exchange?
+    #   render json: {message: "You already have a pending exchange.  Please cancel that one before creating this request"}
+    if @ke.save
+      # redirect_to @ke
       render json: {message: "Successfully created"}, status: 200
     else
       render json: {message: "Something went wrong"}, status: 500
     end
   end
+
+  def destroy
+    ke = KardmaExchange.find_by(id: params[:id])
+
+    if ke.destroy
+      render json: {message: "Successfully deleted"}, status: 200
+    else
+      render json: {message: "Something went wrong"}, status: 500
+    end
+  end
+
 end
