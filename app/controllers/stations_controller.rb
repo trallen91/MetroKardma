188c52e01a322e0b9a831d875fe796b6e8ce202d
Travis Allen <travis.william.allen@gmail.com>
2015-10-28 14:11:04 -0400

    add station method to identify the exchange associated with the current user, and include this station#show response

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -6,10 +6,11 @@ class StationsController < ApplicationController
 
   def show
     @station = Station.find_by(id: params[:id])
+    @station.current_user = current_user
     if params[:user_role] == "swiper"
-      render :json => @station.to_json(:include => {:exchanges_needing_giver => {:include => :receiver}})
+      render :json => @station.to_json(:include => {:exchanges_needing_giver => {:include => :receiver}, :pending_exchange_for_user => {}})
     elsif params[:user_role] == "swipee"
-      render :json => @station.to_json(:include => {:exchanges_needing_receiver => {:include => :giver}})
+      render :json => @station.to_json(:include => {:exchanges_needing_receiver => {:include => :giver}, :pending_exchange_for_user => {}})
     end
   end
 end
