691731f89a3b73dfd928e09d10fe57e19a28b954
Travis Allen <travis.william.allen@gmail.com>
2015-11-12 11:12:56 -0500

    add a ke#update action

--- app/controllers/kardma_exchanges_controller.rb
+++ app/controllers/kardma_exchanges_controller.rb
@@ -5,6 +5,7 @@ class KardmaExchangesController < ApplicationController
     @ke = KardmaExchange.new(station:station)
     role = params[:role]
     @ke.send(role+'=', current_user)
+    @ke.current_user = current_user
 
     if @ke.save
       render json: {message: "Successfully created"}, status: 200
@@ -20,6 +21,19 @@ class KardmaExchangesController < ApplicationController
       render json: {message: "Successfully deleted"}, status: 200
     else
       render json: {message: "Something went wrong"}, status: 500
+    end
+  end
+
+  def update
+    @ke = KardmaExchange.find_by(id: params[:id])
+    role = params[:role]
+    @ke.send(role+'=', current_user)
+    @ke.current_user = current_user
+
+    if @ke.save
+      render json: {message: "Successfully created"}, status: 200
+    else
+      render json: {:errors => @ke.errors.full_messages }
     end
   end
 
