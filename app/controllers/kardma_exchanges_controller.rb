79b537421287f9b1b1c448da785a162413de290c
Max Schwenk <maschwenk@gmail.com>
2015-11-16 00:51:59 -0500

    Find kardma exchanges for user with given role. Had to delete the validation here. I know the validation has meaning but I need to discuss with you how to accommodate it.

--- app/controllers/kardma_exchanges_controller.rb
+++ app/controllers/kardma_exchanges_controller.rb
@@ -13,6 +13,28 @@ class KardmaExchangesController < ApplicationController
     end
   end
 
+
+  def search_by_swiper_swipee
+    @swiper_or_swipee = params[:swiperOrSwipee]
+    if @swiper_or_swipee == 'swiper'
+      @ke = KardmaExchange.where("swiper_id = ?", current_user.id)
+    else
+      @ke = KardmaExchange.where("swipee_id = ?", current_user.id)
+    end
+    render :json => @ke.first
+  end
+
+  def update
+    #Complete this exchange
+    @ke = KardmaExchange.find_by(id: params[:id])
+    @ke.complete = true
+    if @ke.save
+      render json: {message: "Successfully updated"}, status: 200
+    else
+      render json: {:errors => @ke.errors.full_messages }, status: 500
+    end
+  end
+
   def destroy
     ke = KardmaExchange.find_by(id: params[:id])
 
