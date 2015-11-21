435a2a535e792418380b693102df2ba4dfb23831
Travis Allen <travis.william.allen@gmail.com>
2015-11-21 15:49:10 -0500

    render all station information in stations#index, based on user_role

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,7 +1,11 @@
 class StationsController < ApplicationController
   def index
     @stations = Station.all
-    render :json => @stations
+    if params[:user_role] == "swiper"
+      render :json => @stations.to_json(:include => {:exchanges_needing_swiper => {:include => {:swipee => {:methods => :average_rating}}}})
+    elsif params[:user_role] == "swipee"
+      render :json => @stations.to_json(:include => {:exchanges_needing_swipee => {:include => {:swiper => {:methods => :average_rating}}}})
+    end
   end
 
   def show
