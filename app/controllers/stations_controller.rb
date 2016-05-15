96ffb6bcd534fc30f362fccd0e1ab60b26891fe9
Travis Allen <travis.william.allen@gmail.com>
2016-05-15 10:31:26 -0400

    complete merge manually

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,11 +1,7 @@
 class StationsController < ApplicationController
   def index
     @stations = Station.all
-    if params[:user_role] == "swiper"
-      render :json => @stations.to_json(:include => {:exchanges_needing_swiper => {:include => {:swipee => {:methods => :average_rating}}}})
-    elsif params[:user_role] == "swipee"
-      render :json => @stations.to_json(:include => {:exchanges_needing_swipee => {:include => {:swiper => {:methods => :average_rating}}}})
-    end
+    render :json => @stations
   end
 
   def show
